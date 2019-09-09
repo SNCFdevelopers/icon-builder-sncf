c5d7a123677ce2a02211f91f13243eb2f550e6d5
Vincent Desdoigts <v.desdoigts@gmail.com>
2019-09-09 23:44:39 +0200

    Create svg api

--- src/api.controller.ts
+++ src/api.controller.ts
@@ -2,9 +2,11 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { ApiService } from './api.service';
+const copyfiles = require('copyfiles');
 const fs = require('fs');
 const hash = require('object-hash');
 const path = require('path');
+const replace = require('replace-in-file');
 const sharp = require('sharp');
 
 const tmpPath = path.resolve(__dirname, './_tmp');
@@ -78,6 +80,56 @@ export class ApiController {
     });
 
     this.apiService.createArchive(stream, `${output}/pngs/`);
+
+    return;
+  }
+
+  @Get('svg')
+  async findSvg(
+    @Res() res: Response,
+  ) {
+    const id = `svgs${hash(ICON_NAMES)}`;
+    const output = path.join(tmpPath, id);
+    const svgsOutput = path.join(output, 'svgs');
+    const archivePath = path.join(output, 'svg.zip');
+    const iconsSrc = [];
+    const iconsOutput = [];
+
+    // if (fs.existsSync(archivePath)) {
+    //   console.log('The archive already exists.');
+    //   res.download(archivePath);
+    //   return;
+    // }
+
+    this.apiService.createDirectory([tmpPath, output, path.join(output, 'svgs')]);
+    for (let index = 0; index < ICON_NAMES.length; index++) {
+      const icon = `${ICON_NAMES[index]}.svg`;
+      const iconSrc = path.join(svgSrcPath, icon);
+      const iconOutput = path.join(svgsOutput, icon);
+      iconsSrc.push(iconSrc);
+      iconsOutput.push(iconOutput);
+    }
+
+    copyfiles([...iconsSrc, svgsOutput], true, () => {
+      console.log('Files copied');
+      replace({
+        files: iconsOutput,
+        from: 'viewBox="0 0 500 500"',
+        to: 'viewBox="0 0 200 200"',
+      })
+        .then(results => {
+          console.log('Replacement results:', results);
+          const stream = fs.createWriteStream(archivePath);
+          stream.on('close', function() {
+            res.download(archivePath);
+          });
+
+          this.apiService.createArchive(stream, `${output}/svgs/`);
+        })
+        .catch(error => {
+          console.error('Error occurred:', error);
+        });
+    });
 
     return;
   }
