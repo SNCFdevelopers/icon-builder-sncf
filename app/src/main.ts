2fb8c5c5d46942b1e4d4c347477d487ed86f1324
Vincent Desdoigts <v.desdoigts@gmail.com>
2019-09-11 13:59:52 +0200

    Add api rate limit

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 20, // limit each IP to 20 requests per windowMs
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
